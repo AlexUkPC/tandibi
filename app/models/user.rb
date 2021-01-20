dfcb044760d95fe3aed1ef1d6bba45c86271a5c4
AlexUkPC <alexrogna@yahoo.com>
2021-01-20 09:49:13 +0000

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,5 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
   validates :first_name, presence: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address"}
-  har_many :posts
+  has_many :posts
 end
