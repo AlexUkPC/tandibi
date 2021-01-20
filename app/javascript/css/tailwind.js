8c125d1f79922cc8bc7ce416d47cc95547a8357e
AlexUkPC <alexrogna@yahoo.com>
2021-01-20 15:24:37 +0000

    setting up the layout

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,6 +1,17 @@
 module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      }
+    },
   },
   variants: {},
   plugins: [],
