b5f55a87fa6a907ece1f4f7daf114836384ae19f
AlexUkPC <alexrogna@yahoo.com>
2021-01-20 16:55:49 +0000

    use the login field to retrive the user account

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+  devise :database_authenticatable, 
+    :registerable,
+    :recoverable, 
+    :rememberable, 
+    :validatable, 
+    authentication_keys: [:login],
+    reset_password_keys: [:login]
+
   attr_writer :login
 
   validates :email, uniqueness: true
@@ -63,10 +69,30 @@ class User < ApplicationRecord
     where("username = :value OR email = :value", value: login).first
   end
   
-  def self.find_for_database_authentification(condition)
-    conditions = condition.dup
-    login = condition.delete(:login).downcase
+  def self.find_for_database_authentification(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
     find_authenticable(login)
+  end
+
+  def self.send_reset_password_instructions(conditions)
+    recoverable = find_recoverable_or_init_with_errors(conditions)
+
+    if recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+  end
+  
+  def self.find_recoverable_or_init_with_errors(conditions)
+    conditions = conditions.dup
+    login = conditions.delete(:login).downcase
+    recoverable = find_authenticable(login)
+
+    unless recoverable
+      recoverable = new(login: login)
+      recoverable.errors.add(:login, login.present? ? :not_found : :blank)
+    end
+    recoverable
   end
   
   private
