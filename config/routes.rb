e41645ac0f32b8d07f4bb5fff6dfd6a6bcd33759
AlexUkPC <alexrogna@yahoo.com>
2021-01-20 17:12:16 +0000

    config letter opener web

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: "home#index"
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
