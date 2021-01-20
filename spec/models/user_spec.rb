99b7e0ede1b89bd5c97152cbe5e1eed47fda8b51
AlexUkPC <alexrogna@yahoo.com>
2021-01-20 11:21:55 +0000

    updates to use the factory bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,32 +19,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  def create_a_user(email: "#{SecureRandom.hex(4)}@example.org")
-    User.create!(
-      first_name: "Adam",
-      email: email,
-      username: SecureRandom.hex(4),
-    )
-  end
-
+  
   describe "#valid?" do
     it "is valid when email is unique" do
-      user1 = create_a_user
-      user2 = create_a_user
+      user1 = create(:user)
+      user2 = create(:user)
       expect(user2.email).not_to be user1.email
       expect(user2).to be_valid
     end
 
     it "is invalid if the email is taken" do
-      create_a_user(email: "adam@example.org")
+      create(:user, email: "adam@example.org")
       user = User.new
       user.email = "adam@example.org"
       expect(user).not_to be_valid  
     end
 
     it "is invalid if the username is taken" do
-      user = create_a_user
-      another_user = create_a_user
+      user = create(:user)
+      another_user = create(:user)
 
       expect(another_user).to be_valid
       another_user.username = user.username
@@ -52,7 +45,7 @@ RSpec.describe User, type: :model do
     end
 
     it "is invalid if user's first name is blank" do
-      user = create_a_user
+      user = create(:user)
       expect(user).to be_valid
 
       user.first_name = ""
@@ -62,7 +55,7 @@ RSpec.describe User, type: :model do
       expect(user).not_to be_valid  
     end
     it "is invalid if the email looks bogus" do
-      user = create_a_user
+      user = create(:user)
       expect(user).to be_valid
 
       user.email = ""
@@ -89,31 +82,31 @@ RSpec.describe User, type: :model do
   end
   describe "#following" do
     it "can list all of the user's followings" do
-      user = create_a_user
-      friend1 = create_a_user
-      friend2 = create_a_user
-      friend3 = create_a_user
+      user = create(:user)
+      friend1 = create(:user)
+      friend2 = create(:user)
+      friend3 = create(:user)
       
       Bond.create user: user,
-      friend: friend1,
-      state: Bond::FOLLOWING
+        friend: friend1,
+        state: Bond::FOLLOWING
       Bond.create user: user,
-      friend: friend2,
-      state: Bond::FOLLOWING
+        friend: friend2,
+        state: Bond::FOLLOWING
       Bond.create user: user,
-      friend: friend3,
-      state: Bond::REQUESTING
+        friend: friend3,
+        state: Bond::REQUESTING
 
       expect(user.followings).to include(friend1, friend2)
       expect(user.follow_requests).to include(friend3)
     end
     it "can list all of the user's followers" do
-      user1 = create_a_user
-      user2 = create_a_user
-      fol1 = create_a_user
-      fol2 = create_a_user
-      fol3 = create_a_user
-      fol4 = create_a_user
+      user1 = create(:user)
+      user2 = create(:user)
+      fol1 = create(:user)
+      fol2 = create(:user)
+      fol3 = create(:user)
+      fol4 = create(:user)
 
       Bond.create user: fol1,
         friend: user1,
@@ -134,7 +127,7 @@ RSpec.describe User, type: :model do
   end
   describe "#save" do
     it "capitalize the name correctly" do
-      user = create_a_user
+      user = create(:user)
 
       user.first_name = "AdaM"
       user.last_name = "van der Berg"
