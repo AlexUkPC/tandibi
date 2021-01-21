7e91c7d6c4d01b201ad13fbac675e39d5a4e8352
AlexUkPC <alexrogna@yahoo.com>
2021-01-21 18:44:42 +0000

    moved humanized method from helper to decorator

---
+++ app/decorators/sight_decorator.rb
@@ -0,0 +1,10 @@
+# frozen_sight_literal: true
+
+module SightDecorator
+  def humanized_activity_type
+    case activity_type
+    when Sight::CHECKIN then "checked in at"
+    when Sight::CHECKOUT then "checked out from"
+    end
+  end
+end
