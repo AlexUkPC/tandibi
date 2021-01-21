396801e7bd20cc9e905ac77e4fb050c780e9e0f2
AlexUkPC <alexrogna@yahoo.com>
2021-01-21 10:38:48 +0000

    register path for timeline controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'timelines/index'
-  get 'timelines/show'
   devise_for :users
   get 'home/index'
   root to: "home#index"
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
+  authenticate :user do
+    resources :timeline,
+      only: [:index, :show]
   end
 end
