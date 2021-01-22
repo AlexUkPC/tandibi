3810bd7007f33d0691998f3cb14014d2526c95fa
AlexUkPC <alexrogna@yahoo.com>
2021-01-22 16:19:59 +0000

    interchange post_id with caption not null condition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_105527) do
+ActiveRecord::Schema.define(version: 2021_01_22_161734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,8 @@ ActiveRecord::Schema.define(version: 2021_01_22_105527) do
   end
 
   create_table "pictures", force: :cascade do |t|
-    t.bigint "post_id"
-    t.string "caption", null: false
+    t.bigint "post_id", null: false
+    t.string "caption"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
