9ebce46bbb4db74af66ae8c7da2644bad62f3512
AlexUkPC <alexrogna@yahoo.com>
2021-01-23 13:42:12 +0000

    create actions for vuex

---
+++ app/javascript/store/actions.js
@@ -0,0 +1,9 @@
+function handleError(commit, error) {
+    const message = error.message || error.info.error_description
+    commit("setError", message)
+}
+const actions = {
+
+}
+
+export default actions
