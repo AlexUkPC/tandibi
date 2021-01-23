33eafbd0486ca18675b46afabc57b477f8daca82
AlexUkPC <alexrogna@yahoo.com>
2021-01-23 19:04:55 +0000

    setup PlaceCrawler job

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PlacesController < ApplicationController
     keyword = params.fetch(:keyword)
     lat = params.fetch(:lat)
     lng = params.fetch(:lng)
-    Place::Crawler.call(keyword, lat: lat, lng: lng)
+    PlaceCrawlerJob.perform_later(keyword, lat: lat, lng: lng)
     render json: Place::Finder.call(keyword, lat: lat, lng: lng)
   end
 end
