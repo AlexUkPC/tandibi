ef6d685f7afd22a43cf49b9bb02c9221d460579a
AlexUkPC <alexrogna@yahoo.com>
2021-01-23 15:51:11 +0000

    wiring things up

--- app/javascript/store/mutations.js
+++ app/javascript/store/mutations.js
@@ -3,8 +3,34 @@ const mutations = {
         state.error = error
     },
 
+    SET_COORDINATES(state, [lng, lat]){
+        state.coordinates.lng = lng
+        state.coordinates.lat = lat
+    },
+
+    SET_SIGHT_KEYWORD(state, kw){
+        state.sight.keyword = kw
+    },
+
+    SET_SIGHT_PALCES(state, places){
+        state.sight.places = places
+    },
+
+    SET_SIGHT_SELECTED_PALCE(state, places){
+        state.sight.selectedPlace = place
+    },
+
     RESET(state){
         state.error = null
+        state.coordinates = {
+            lat: null,
+            lng: null,
+        }
+        state.sight = {
+            keyword: "",
+            places: [],
+            selectedPlace: null,
+        }
     }
 }
 
