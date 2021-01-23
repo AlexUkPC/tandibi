8ab2b24b3948eb9a00741588359256f2d0aa0e1f
AlexUkPC <alexrogna@yahoo.com>
2021-01-23 13:42:06 +0000

    create index for vuex

---
+++ app/javascript/store/index.js
@@ -0,0 +1,30 @@
+import Vue from "vue"
+import Vuex from "vuex"
+import actions from "./actions"
+import mutations from "./mutations"
+
+Vue.use(Vuex)
+
+const isDev = process.env.NODE_ENV !== "production"
+
+const store = new Vuex.Store({
+    state: {
+        error: null,
+    },
+    
+    // the only way too change the state
+    mutations,
+
+    // perform async actions
+    actions,
+
+    // value derived from the state
+    getters: {
+        hasError: state => state.error ? true : false
+    },
+
+    // raise an error if a state is unlawfully mutated
+    strict: isDev
+})
+store.commit("RESET")
+export default store
