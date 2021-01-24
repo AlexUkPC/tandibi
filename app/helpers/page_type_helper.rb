3167958fce85e35c6c6ec945021dd6f9f8989774
AlexUkPC <alexrogna@yahoo.com>
2021-01-24 14:35:30 +0000

    acepting and rejecting requests

--- app/helpers/page_type_helper.rb
+++ app/helpers/page_type_helper.rb
@@ -8,10 +8,21 @@ module PageTypeHelper
   end
   
   def on_self?
-    on_timeline? && action_name == "show" && params[:username] == current_user.username
+    on_timeline? && action_name == "show" &&
+      params[:username] == current_user.username
   end
   
   def on_settings?
     controller_path.start_with? "settings"
+  end
+
+  def on_following_list?
+    controller_path == "bonds" &&
+      action_name == "following"
+  end
+  
+  def on_follower_list?
+    controller_path == "bonds" &&
+      action_name == "followers"
   end
 end
